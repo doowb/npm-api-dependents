90bcdf668580e42866f92573a6f38aa660ab0d2e
Brian Woodward <brian.woodward@gmail.com>
2016-03-23 17:27:29 -0400

    example comments

--- examples/map-fn.js
+++ examples/map-fn.js
@@ -10,6 +10,10 @@ var repo = npm.repo('micromatch');
 // use with co
 co(function* () {
   var repos = yield repo.dependents({
+    /**
+     * Use a custom function to transform the repo name
+     * into a `Repo` object from `npm-api`
+     */
     mapFn: function(name) {
       return this.repo(name);
     }
