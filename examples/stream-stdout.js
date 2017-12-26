17a348fc81a86428baf6bf17bd0a154e2c7acaef
Lars-Magnus Skog <ralphtheninja@riseup.net>
2017-12-26 20:47:32 +0100

    repo -> obj for parameter

--- examples/stream-stdout.js
+++ examples/stream-stdout.js
@@ -6,7 +6,7 @@ var dependents = require('../');
 npm.use(dependents());
 
 var repo = npm.repo('micromatch');
-repo.dependents().pipe(through2.obj(function (repo, enc, cb) {
-  this.push(repo.name + '\n');
+repo.dependents().pipe(through2.obj(function (obj, enc, cb) {
+  this.push(obj.name + '\n');
   cb();
 })).pipe(process.stdout);
